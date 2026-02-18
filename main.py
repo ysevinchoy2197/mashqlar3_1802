aa62b4c596b6c962acaf9cc9d8c59d72890b140b
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-02-18 16:43:47 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+# #1-misol
+# numbers = [12, -5, 7, -3, 0, 25, -11, 8]
+# print(numbers)
+#
+# numbers.remove(-5)
+# numbers.remove(-3)
+# numbers.remove(-11)
+
+# #2-misol
+# numbers = [45, 12, 78, 3, 56, 89, 23]
+# print(numbers)
+#
+# print(max(numbers))
+# print(min(numbers))
+
+# #3-misol
+# numbers = [1, 2, 3, 4, 5, 6]
+# print(numbers)
+# numbers.reverse()
+# print(numbers)
+
+# #5-misol
+# list1 = [1, 2, 3, 4, 5]
+# list2 = [4, 5, 6, 7, 8]
+# print(list1)
+# print(list2)
+# print(list1 + list2)
+# print()
+
+# #6-misol
+# numbers = [10, 20, 30, 40, 50]
+# print(numbers)
+#
+# sum(numbers)
+# print(sum(numbers))
+
+# #7-misol
+# numbers = [5, 3, 7, 3, 9, 3, 1]
+# print(numbers)
+#
+# print(numbers.count(3))
+
+# #9-misol
+# numbers = [29, 10, 14, 37, 13]
+# print(numbers)
+#
+# numbers.sort()
+# print(numbers)
+
+# #10-misol
+# numbers = [4, 6, 2, 6, 8, 4, 6, 2]
+# print(numbers)
+#
+# count_dict = {}
+#
+# for num in numbers:
+#     if num in count_dict:
+#         count_dict[num] += 1
+#     else:
+#         count_dict[num] = 1
+#
+# print("Takrorlanish sonlari:", count_dict)
